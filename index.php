16027e7104481d75ca6ee26fc36d5eb3a5a54657
Verona <hilary@codebloo.com>
2018-03-22 17:50:32 -0700

    Update index.php
    adding in the loop

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
-
-<section>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <section class="post">
 <h2><?php the_title();?></h2>
 <?php the_content(); ?>
 </section>
+	
+<?php endwhile; endif; ?>
 
 <?php get_footer();?>
